92bab6e7ab8202bf995465fe4ccfc631f20e8299
Do you know the way? <127231571+Sergi-sanPerez@users.noreply.github.com>
2023-03-23 18:35:32 +0100

    Update examen.java

--- examen.java
+++ examen.java
@@ -1,4 +1,4 @@
-public class Main {
+public class examen {
         public static void main(String[] args){
                 System.out.println("Soc Sergi Pérez Segalà i estic fent l’examen de la UF2 del M01");
         }
