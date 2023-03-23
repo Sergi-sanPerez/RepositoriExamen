dc4db51d9b711c910edb2e14b39f8fe8094cd192
Do you know the way? <127231571+Sergi-sanPerez@users.noreply.github.com>
2023-03-23 18:29:47 +0100

    examen.java

---
+++ examen.java
@@ -0,0 +1,5 @@
+public class Main {
+        public static void main(String[] args){
+                System.out.println("Soc Sergi Pérez Segalà i estic fent l’examen de la UF2 del M01");
+        }
+}
